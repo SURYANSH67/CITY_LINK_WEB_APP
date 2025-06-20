ca34536eec8403c5bb6ad891e9e57db7bda600b6
SURYANSH67 <159783188+SURYANSH67@users.noreply.github.com>
2025-06-20 19:23:44 +0530

    Update run.py

--- report-it-now-flask/run.py
+++ report-it-now-flask/run.py
@@ -9,8 +9,3 @@ from app import create_app
 
 # Create the app instance so the FLASK_APP system can find it
 app = create_app()
-
-
-# This block is only for running the development server directly
-if __name__ == '__main__':
-    app.run(debug=True, port=5001)
